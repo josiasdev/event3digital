394db1873a1eabdb9170f283d6d31de7ba96a4ae
josiasdev <josiasmartins098@gmail.com>
2024-11-17 16:26:23 -0300

    feat: testando o next.js com o typescript

---
+++ frontEnd/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <>
+            <p>Este é um exemplo básico de página com Next.js e TypeScript.</p>
+        </>
+    );
+};
+
+export default Header;
